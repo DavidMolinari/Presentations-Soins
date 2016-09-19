f0ff36f118aed48b7f38524563038ecee4e8001c
MOLINARI David <molinari.d@sio.lan>
2016-09-19 09:01:43 +0200

    Base de PresSoin

---
+++ PresSoins/Prestation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesMetier
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Prestation
+    {
+        public string Libelle { get; }
+        public DateTime DateSoin { get; }
+        public DateTime HeureSoin { get; }
+        public Intervenant L_intervenant { get; }
+
+        /// <summary>
+        /// Constructeur de la classe Prestation
+        /// </summary>
+        /// <param name="libelle"></param>
+        /// <param name="dateSoin"></param>
+        /// <param name="heureSoin"></param>
+        /// <param name="intervenant"></param>
+        public Prestation(string libelle, DateTime dateSoin,DateTime heureSoin, Intervenant intervenant )
+        {
+            this.Libelle = libelle;
+            this.DateSoin = dateSoin;
+            this.HeureSoin = heureSoin;
+            this.L_intervenant = intervenant;
+        }
+
+    }
+}
